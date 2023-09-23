f5a2e7cefafdee6292c05ffee4d47b679a9ea3aa
menghan <menghan.bai@thoughtworks.com>
2023-09-23 21:45:22 +0800

    feat: add player

--- static/js/635.56f4ba9b.chunk.js
+++ static/js/635.56f4ba9b.chunk.js
@@ -1,2 +1,10 @@
-"use strict";(self.webpackChunkstory_card=self.webpackChunkstory_card||[]).push([[635],{635:function(s,c,e){e.r(c),c.default={}}}]);
+"use strict";
+(self.webpackChunkstory_card = self.webpackChunkstory_card || []).push([
+  [635],
+  {
+    635: function (s, c, e) {
+      e.r(c), (c.default = {});
+    },
+  },
+]);
 //# sourceMappingURL=635.56f4ba9b.chunk.js.map
